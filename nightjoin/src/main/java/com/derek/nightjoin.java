e727d2c37c70bc618f2d6f9ebb4829419d3706a8
Derek <82894246+LaDerek@users.noreply.github.com>
2021-09-04 02:06:14 +0300

    Added timer
    ver 2.0

--- nightjoin/src/main/java/com/derek/nightjoin.java
+++ nightjoin/src/main/java/com/derek/nightjoin.java
@@ -1,12 +1,14 @@
 package com.derek;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import java.util.logging.Logger;
 
 public class nightjoin extends JavaPlugin implements Listener{
 
@@ -15,19 +17,26 @@ public class nightjoin extends JavaPlugin implements Listener{
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        nightjoin._log.info("Плагин включён!");
+        nightjoin._log.info("Plugin is enabled!");
     }
 
     @Override
     public void onDisable() {
-        nightjoin._log.info("Плагин отключён!");
+        nightjoin._log.info("Plugin is disabled!");
     }
 
     @EventHandler
     public void onLogin(PlayerLoginEvent e){
-        if(!e.getPlayer().hasPermission("nightjoin.join")){
-            e.setResult(PlayerLoginEvent.Result.KICK_OTHER);
-            e.setKickMessage(ChatColor.RED + "У вас нет прав для игры на сервере!");
-        }
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+            @Override
+            public void run() {
+                for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                    if (!e.getPlayer().hasPermission("nightjoin.join")) {
+                        p.kickPlayer(ChatColor.RED + "У вас нет прав для игры на сервере!");
+                    }
+                }
+            }
+        }, 10);
     }
 }
